a585b88199bb49e203d420cd1b7f17b3519720cd
MoazAbdullah1 <moazabdullah02@gmail.com>
2023-09-15 00:22:28 +0500

    Lec 3

---
+++ Lec 3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {       
+  public:        
+    int Rollno;  
+    string Regno;
+    string Tsub;
+    
+    MyClass( ){
+    	Rollno = 10;
+    	Regno = "FA 22";
+    	Tsub = "Eng, Urdu";
+	}
+	
+	MyClass(int r, string g, string t){
+		Rollno = r;
+		Regno = g;
+		Tsub = t;
+	}
+};
+
+int main() {
+	MyClass class1, class2(22, "Sp 33", "ICT, math");
+	
+	cout << class1.Rollno << endl;
+	cout << class1.Regno << endl;
+	cout << class1.Tsub << endl;
+	
+	cout << class2.Rollno << endl;
+	cout << class2.Regno << endl;
+	cout << class2.Tsub << endl;
+
+  return 0;
+  
+}
