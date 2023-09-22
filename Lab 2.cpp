cd242934aaa9e6ef138830777a36bcc8f117c447
MoazAbdullah1 <moazabdullah02@gmail.com>
2023-09-22 11:05:50 +0500

    initial Comment

--- Lab 2.cpp
+++ Lab 2.cpp
@@ -1,40 +1,6 @@
 #include <iostream>
 using namespace std;
 
-//class Calculator {
-//public:
-//    double value;
-//
-//    Calculator(double initialValue) : value(initialValue) {}
-//
-//    Calculator& add(double other) {
-//        value += other;
-//        return *this;
-//    }
-//
-//    Calculator& multiply(double other) {
-//        value *= other;
-//        return *this;
-//    }
-//
-//    Calculator& power(double exponent) {
-//        value = pow(value, exponent);
-//        return *this;
-//    }
-//};
-//
-//int main() {
-//    Calculator calc(2);
-//    calc.add(3).multiply(4).power(2);
-//    double result = calc.value;
-//    cout << "Result: " << result << endl; // Output: Result: 100
-//    return 0;
-//}
-//
-//
-
-using namespace std;
-
 class MyCal { 
       
   public:        
